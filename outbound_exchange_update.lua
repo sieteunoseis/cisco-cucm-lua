86e61d7f48af7473977daf40ae45a1066a914e69
Jeremy Worden <jeremy.worden@gmail.com>
2022-09-06 13:15:53 -0700

    Create outbound_exchange_update.lua

---
+++ outbound_exchange_update.lua
@@ -0,0 +1,19 @@
+M={}
+
+function M.outbound_INVITE(msg)
+
+  local from = msg:getHeader("From")  --Get FROM Header
+  local to = msg:getHeader("To")  --Get To Header
+  local dummy,userStart = string.find(from, "sip:")
+  local areaExchange = string.sub(from, userStart+3, userStart+5) --Extract caller area exchange
+  local newExchange = string.gsub(to,"999",areaExchange) --Update TO Header with extracted area exchange
+
+  msg:modifyHeader("To", newExchange)  --Replace TO Header
+  local method, ruri, ver = msg:getRequestLine()
+  local uri = string.gsub (ruri, "999",areaExchange)
+
+  msg:setRequestUri(uri)
+
+end
+
+return M
